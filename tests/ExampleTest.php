e9a17d29af8d23313e04b4cb9ce31cc566bfdeba
Keunbae Park <ppotpo@gmail.com>
2019-03-09 10:36:37 -0500

    wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,10 +5,4 @@ namespace Pkboom\Skeleton\Tests;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
-{
-    /** @test */
-    public function true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+{ }
